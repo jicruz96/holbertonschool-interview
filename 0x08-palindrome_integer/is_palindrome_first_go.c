21cd002972b1f2e0f15e77a7f068c8a179babb77
jicruz96 <accounts@jicruz.com>
2021-02-11 13:42:57 +0000

    Include version of previous algorithm

---
+++ 0x08-palindrome_integer/is_palindrome_first_go.c
@@ -0,0 +1,41 @@
+#include "palindrome.h"
+#include <limits.h>
+
+/**
+ * is_palindrome - checks if an unsigned integer is a palindrome
+ * @n: number to check
+ * Return: 1 if is palindrome | 0 if not
+ **/
+int is_palindrome(unsigned long n)
+{
+	unsigned long shaver = 1;
+
+	/* prevent division by zero errors */
+	if (n < 10)
+		return (1);
+
+	/* handles limit edge case */
+	if (n == ULONG_MAX)
+		return (0);
+
+	/* get divisor to same magnitude as n */
+	while (n / shaver)
+		shaver *= 10;
+	shaver /= 10;
+
+	/* verify palindrome-ness */
+	while (n >= 10)
+	{
+		/* last digit (n%10) must equal first (n/shaver) */
+		if (n % 10 != n / shaver)
+			return (0);
+
+		/* remove first and last digits */
+		n = (n % shaver) / 10;
+
+		/* re-size shaver to n's new magnitude */
+		shaver /= 100;
+	}
+
+	return (1);
+}
