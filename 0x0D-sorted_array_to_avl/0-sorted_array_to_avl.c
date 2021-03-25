addd886672eaa981254e6a40b33578783d90f715
J.I. Cruz <acounts@jicruz.com>
2021-03-25 16:56:16 -0400

    Add 0x0D

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * create_node - creates AVL tree node
+ * @value: value of node
+ * @parent: node parent
+ * Return: node
+ **/
+avl_t *create_node(int value, avl_t *parent)
+{
+	avl_t *node = malloc(sizeof(avl_t));
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
+
+/**
+ * sorted_array_to_avl - sorted array to avl
+ * @array: array
+ * @size: size
+ * Return: tree
+ **/
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	return (helper(array, 0, size - 1, NULL));
+}
+
+/**
+ * helper - helper
+ * @array: array
+ * @start: start
+ * @end: end
+ * @parent: parent
+ * Return: tree
+ **/
+avl_t *helper(int *array, int start, int end, avl_t *parent)
+{
+	int mid = (start + end) / 2;
+	avl_t *node = NULL;
+
+	if (start <= end)
+	{
+		node = create_node(array[mid], parent);
+		node->left = helper(array, start, mid - 1, node);
+		node->right = helper(array, mid + 1, end, node);
+	}
+	return (node);
+}
---
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -0,0 +1,33 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+typedef struct binary_tree_s avl_t;
+
+
+void binary_tree_print(const binary_tree_t *);
+avl_t *helper(int *array, int start, int end, avl_t *parent);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *create_node(int value, avl_t *parent);
+void print_array(const int *array, size_t size);
+
+
+
+#endif /* _BINARY_TREES_H_ */
