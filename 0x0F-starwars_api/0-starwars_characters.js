fd16c094b098f3c8264841e58aeae6ad7fde3589
jicruz96 <accounts@jicruz.com>
2021-04-12 02:14:47 +0000

    Second go at API problem

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,30 +2,20 @@
 
 // Import the request module
 const request = require('request');
+const URL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
 
-const filmId = process.argv[2]; // filmId is the third argument
-const URL = 'https://swapi-api.hbtn.io/api/films/';
-
-function action (_error, _response, body) {
-  const episodeId = JSON.parse(body).episode_id;
-
-  request(URL, function (_error, _response, body) {
-    let i;
-    const info = JSON.parse(body);
-
-    for (i = 0; i < info.count; i++) {
-      if (info.results[i].episode_id === episodeId) { break; }
-    }
-
-    const characters = info.results[i].characters;
-
-    for (i = 0; i < characters.length; i++) {
-      request(characters[i], function (_error, _response, body) {
-        console.log(JSON.parse(body).name);
-      });
-    }
+async function getCharacterName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (_err, _res, body) => { resolve(JSON.parse(body).name); });
   });
 }
 
-// Make the request
-request(URL + `${filmId}/`, action);
+async function printCharacters (_error, _response, body) {
+  for (const url of JSON.parse(body).characters) {
+    const characterName = await getCharacterName(url);
+
+    console.log(characterName);
+  }
+}
+
+request(URL, printCharacters);
