127b7954bf35c3629e377a42c58b7648917ecbd5
J.I. Cruz <acounts@jicruz.com>
2021-04-29 22:51:59 -0500

    Initial commit of project folder 0x12

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * print_int_array - prints an array of integers
+ * @array: array of integers
+ * @size: size of array
+ **/
+void print_int_array(int *array, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+		printf("%s%d", i ? ", " : "", array[i]);
+
+	putchar('\n');
+}
+/**
+ * advanced_binary - advanced binary search
+ * @array: SORTED array of integers
+ * @size: size of array
+ * @value: value to find
+ * Return: index of first appearance of value
+ **/
+int advanced_binary(int *array, size_t size, int value)
+{
+	int i;
+	int mid;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	print_int_array(array, size);
+
+	i = (size / 2) - (size % 2 == 0);
+	mid = i;
+	if (array[mid] < value)
+	{
+		i = advanced_binary(array + i + 1, size - i - 1, value);
+		if (i == -1)
+			return (-1);
+		return (mid + i + 1);
+	}
+
+	if (array[mid] == value && mid == 0)
+		return (mid);
+
+	i = advanced_binary(array, size - i - 1, value);
+
+	if (array[mid] > value || (i >= 0 && i < mid))
+		return (i);
+
+	return (mid);
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+typedef unsigned long size_t;
+
+int advanced_binary(int *array, size_t size, int value);
+void print_int_array(int *array, size_t size);
+
+
+#endif /* SEARCH_ALGOS_H */
