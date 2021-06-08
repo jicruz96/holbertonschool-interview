92370e96474a4ecc792cc4405242425373b7d52e
jicruz96 <accounts@jicruz.com>
2021-06-08 05:51:35 -0500

    refactor 0x18

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,6 +1,4 @@
 #include "sort.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * merge_sort - implementation of merge sort algorithm
@@ -14,58 +12,42 @@ void merge_sort(int *array, size_t size)
 	merge_sort_helper(array, size, tmp);
 	free(tmp);
 }
+
 /**
  * merge_sort_helper - helper for merge_sort
  * @array: array of integers
  * @size: size of array
- * @tmp: tmp pointer
+ * @tmp: tmp array
  */
 void merge_sort_helper(int *array, size_t size, int *tmp)
 {
-	size_t half_size;
+	size_t size_l = size / 2, size_r = size - size_l, i = 0, j = 0, k = 0;
+	int *right = array + size_l;
 
 	if (size <= 1)
 		return;
 
-	half_size = size / 2;
-	merge_sort_helper(array, half_size, tmp);
-	merge_sort_helper(array + half_size, size - half_size, tmp);
-	merger(array, half_size, array + half_size, size - half_size, tmp);
-}
-
-
-/**
- * merger - helper for merge_sort
- *
- * @l: left array
- * @size_l: size of left array
- * @r: right array
- * @size_r: size of right array
- * @tmp: tmp array
- */
-void merger(int *l, size_t size_l, int *r, size_t size_r, int *tmp)
-{
-	size_t i = 0, j = 0, k = 0;
+	merge_sort_helper(array, size_l, tmp);
+	merge_sort_helper(right, size_r, tmp);
 
 	for (i = 0; i < size_l; i++)
-		tmp[i] = l[i];
+		tmp[i] = array[i];
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(l, size_l);
+	printf("Merging...\n[left]: ");
+	print_array(array, size_l);
 	printf("[right]: ");
-	print_array(r, size_r);
+	print_array(right, size_r);
 
-	for (i = 0; i < size_l && j < size_r; l[k++] = tmp[i], i++)
-		for (; j < size_r && r[j] < tmp[i]; j++)
-			l[k++] = r[j];
+	i = 0;
+	while (i < size_l || j < size_r)
+	{
+		while (j < size_r && (i >= size_l || right[j] < tmp[i]))
+			array[k++] = right[j++];
 
-	for (; i < size_l; i++)
-		l[k++] = tmp[i];
-
-	for (; j < size_r; j++)
-		l[k++] = r[j];
+		while (i < size_l && (j >= size_r || tmp[i] <= right[j]))
+			array[k++] = tmp[i++];
+	}
 
 	printf("[Done]: ");
-	print_array(l, size_l + size_r);
+	print_array(array, size);
 }
