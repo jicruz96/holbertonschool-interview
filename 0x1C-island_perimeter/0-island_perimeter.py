6301853bb4ff549aa8d11778c41fca2a3a2020b7
jicruz96 <>
2021-07-13 17:54:59 +0000

    Initial commit of project folder 0x1C

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+See problem description in README.md
+"""
+
+
+def cell_perimeter(grid, i, j):
+    """
+    Calculates the perimeter of a cell in a rectangular grid
+    """
+    # empty cell, no perimeter
+    if grid[i][j] == 0:
+        return 0
+    
+    perimeter = 0
+
+    # top cell
+    if i > 0 and grid[i - 1][j] == 0:
+        perimeter += 1
+    # bottom cell
+    if i < len(grid) - 1 and grid[i + 1][j] == 0:
+        perimeter += 1
+    # left cell
+    if j > 0 and grid[i][j - 1] == 0:
+        perimeter +=  1
+    # right cell
+    if j < len(grid[i]) - 1 and grid[i][j - 1] == 0:
+        perimeter += 1
+    
+    return perimeter
+    
+
+def solution1(grid):
+    """O(n^2) solution"""
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            perimeter += cell_perimeter(grid, i, j)
+    
+    return perimeter
+                
+
+
+def island_perimeter(grid):
+    """Calculates the perimeter of an "island" represented by 1's on a rectangular grid of zeroes"""
+    return solution1(grid)
---
+++ 0x1C-island_perimeter/README.md
@@ -0,0 +1,19 @@
+# Interview Prep: Island Perimeter
+
+## Description of Problem
+
+Create a function `def island_perimeter(grid):` that returns the perimeter of the island described in `grid`:
+
+* `grid` is a list of list of integers:
+    * 0 represents water
+    * 1 represents land
+* Each cell is square, with a side length of 1
+* Cells are connected horizontally/vertically (not diagonally).
+* `grid` is rectangular, with its width and height not exceeding 100
+* The grid is completely surrounded by water
+* There is only one island (or nothing).
+* The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+
+## Proposed Solution 💡
+
+See [0-island_perimeter.py](./island_perimeter.py)
