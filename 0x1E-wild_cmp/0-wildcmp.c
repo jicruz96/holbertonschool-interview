585ae57b1b797b3fc7d0141af415b8319aea677c
jicruz96 <accounts@jicruz.com>
2021-07-26 03:29:23 -0500

    Refactor solution

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 #ifndef NULL
 #define NULL ((void *)0)
 #endif
@@ -16,43 +16,14 @@ int wildcmp(char *s1, char *s2)
 	if (!s1 || !s2)
 		return (0);
 
-	if (*s1 == '\0')
-	{
-		while (*s2)
-			if (*(s2++) != '*')
-				return (0);
-
-		return (1);
-	}
+	if (*s1 == *s2 && *s1 != '*')
+		return (*s1 == '\0' || wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 != '*')
-		return (*s1 == *s2 && wildcmp(s1 + 1, s2 + 1));
+		return (0);
 
-	while (*s2 == '*')
-		if (*(++s2) == '\0')
-			return (1);
+	if (s2[1] == '*' || s2[1] == *s2)
+		return (wildcmp(s1, s2 + 1));
 
-	for (s1 = _strchr(s1, *s2); s1; s1 = _strchr(s1 + 1, *s2))
-		if (wildcmp(s1, s2))
-			return (1);
-
-	return (0);
-}
-
-/**
- * _strchr - custom implementation of strchr
- * @str: string to search for a char `chr` for
- * @chr: char to search for in string
- * Return: pointer to first instance of chr in str
- **/
-char *_strchr(char *str, char chr)
-{
-	if (!str)
-		return (NULL);
-
-	for (; *str; str++)
-		if (*str == chr)
-			return (str);
-
-	return (chr == '\0' ? str : NULL);
+	return (*s1 && wildcmp(s1 + 1, s2));
 }
