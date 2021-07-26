4e5b99362297687acb33e184c8bf5ccb002be1bc
jicruz96 <accounts@jicruz.com>
2021-07-26 03:49:28 -0500

    refactor solution

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -13,17 +13,18 @@
  **/
 int wildcmp(char *s1, char *s2)
 {
-	if (!s1 || !s2)
-		return (0);
-
-	if (*s1 == *s2 && *s1 != '*')
-		return (*s1 == '\0' || wildcmp(s1 + 1, s2 + 1));
-
-	if (*s2 != '*')
-		return (0);
-
-	if (s2[1] == '*' || s2[1] == *s2)
+	if (*s2 == '*')
+	{
+		if (*s1 && wildcmp(s1 + 1, s2))
+			return (1);
 		return (wildcmp(s1, s2 + 1));
+	}
 
-	return (*s1 && wildcmp(s1 + 1, s2));
+	if (*s1 != *s2)
+		return (0);
+
+	if (!*s1)
+		return (1);
+
+	return (wildcmp(s1 + 1, s2 + 1));
 }
