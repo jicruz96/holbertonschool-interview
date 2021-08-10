6e596ef03616e296162f803207b1dc560a14af1d
jicruz96 <accounts@jicruz.com>
2021-08-10 16:47:16 -0400

    Debug 0-island_perimeter.py

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -15,16 +15,16 @@ def cell_perimeter(grid, i, j):
     perimeter = 0
 
     # top cell
-    if i > 0 and grid[i - 1][j] == 0:
+    if i == 0 or grid[i - 1][j] == 0:
         perimeter += 1
     # bottom cell
-    if i < len(grid) - 1 and grid[i + 1][j] == 0:
+    if i == len(grid) - 1 or grid[i + 1][j] == 0:
         perimeter += 1
     # left cell
-    if j > 0 and grid[i][j - 1] == 0:
+    if j == 0 or grid[i][j - 1] == 0:
         perimeter += 1
     # right cell
-    if j < len(grid[i]) - 1 and grid[i][j - 1] == 0:
+    if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
         perimeter += 1
 
     return perimeter
