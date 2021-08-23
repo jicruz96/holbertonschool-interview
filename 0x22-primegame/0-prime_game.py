d30137b2b9f409f2d9e669e99a653d2845e9a38f
jicruz96 <accounts@jicruz.com>
2021-08-23 18:18:04 -0400

    update 0x22

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -17,22 +17,22 @@ def isWinner(x, nums):
     def is_prime(num):
         return num in primes
 
-    def number_of_primes_below_num(num):
+    def number_of_primes_below_or_equal_to_num(num):
         num_primes = 0
         for i in range(2, num + 1):
             num_primes += is_prime(i)
         return num_primes
 
     def is_even(n):
-        return n % 2
+        return n % 2 == 0
 
     maria_score = 0
     ben_score = 0
     while x > 0:
-        if is_even(number_of_primes_below_num(nums.pop(0))):
-            maria_score += 1
-        else:
+        if is_even(number_of_primes_below_or_equal_to_num(nums.pop(0))):
             ben_score += 1
+        else:
+            maria_score += 1
         x -= 1
 
     if maria_score > ben_score:
